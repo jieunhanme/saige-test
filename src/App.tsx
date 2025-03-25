e5c88b2972d5decca7e02c700c352a6c8eead32e
jieunhanme <jieun93.h@gmail.com>
2025-03-25 20:26:00 +0900

    chore: add dependencies(antd, tailwind)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <div>
-      이곳에 To-Do App을 작성해주세요.
-    </div>
-  )
+  return <div className="text-blue-500">이곳에 To-Do App을 작성해주세요.</div>
 }
 
 export default App
