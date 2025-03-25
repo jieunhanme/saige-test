37ee2b27f06b7930f60bbf8a4e088ef6432f9bc9
jieunhanme <jieun93.h@gmail.com>
2025-03-25 23:52:47 +0900

    style: setting color palette and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Button, ConfigProvider } from 'antd'
 import './App.css'
 
 function App() {
-  return <div className="text-blue-500">이곳에 To-Do App을 작성해주세요.</div>
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#FF4052',
+          borderRadius: 4,
+          fontFamily: 'MinSans',
+        },
+      }}
+    >
+      <div className="text-saige-dark-grey">
+        이곳에 To-Do App을 작성해주세요.
+      </div>
+      <Button type="primary">버튼</Button>
+    </ConfigProvider>
+  )
 }
 
 export default App
