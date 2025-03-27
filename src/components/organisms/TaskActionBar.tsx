bb930f704c82e30ac6caf1a84623b882a0b80ae1
jieunhanme <jieun93.h@gmail.com>
2025-03-27 22:49:21 +0900

    feat: todos 변형을 위한 TaskActionBar 컴포넌트 생성

---
+++ src/components/organisms/TaskActionBar.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { Col, Row } from 'antd'
+
+import { AddButton, RemoveButton } from '../atoms'
+import { TaskForm } from '../molecules'
+import { ToDoRequest } from '../../types/api'
+
+type TaskActionBarProps = {
+  handleAddTask: (values: ToDoRequest) => void
+}
+
+export function TaskActionBar({ handleAddTask }: TaskActionBarProps) {
+  const [isAdding, setIsAdding] = useState<boolean>(false)
+  const [isRemoving, setIsRemoving] = useState<boolean>(false)
+
+  const handleClickAdd = () => {
+    setIsAdding(true)
+    setIsRemoving(false)
+  }
+
+  const handleClickRemove = () => setIsRemoving((flag) => !flag)
+
+  const handleSubmit = (values: ToDoRequest) => {
+    handleAddTask(values)
+    setIsAdding(false)
+  }
+  const handleCancelSubmit = () => {
+    setIsAdding(false)
+  }
+
+  return (
+    <section className="pt-4 px-4">
+      {!isAdding ? (
+        <Row gutter={8}>
+          <Col flex="auto">
+            <AddButton onClick={handleClickAdd} />
+          </Col>
+          <Col>
+            <RemoveButton isRemoving={isRemoving} onClick={handleClickRemove} />
+          </Col>
+        </Row>
+      ) : (
+        <TaskForm
+          handleSubmit={handleSubmit}
+          handleCancelSubmit={handleCancelSubmit}
+        />
+      )}
+    </section>
+  )
+}
---
+++ src/components/organisms/index.tsx
@@ -0,0 +1,1 @@
+export { TaskActionBar } from './TaskActionBar'
